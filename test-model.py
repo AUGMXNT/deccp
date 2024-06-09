fc5374e4145fd7ea8645dc1b4a47c847e427eb7e
lhl <lhl@randomfoo.net>
2024-06-10 03:19:29 +0900

    runs through all censored prompts

--- test-model.py
+++ test-model.py
@@ -1,11 +1,23 @@
 import torch
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model_path = "modified_model"
 model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, device_map="auto", torch_dtype=torch.bfloat16)
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
-generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
-prompt = "Once upon a time"
-output = generator(prompt, max_length=200, num_return_sequences=1)
-print(output[0]['generated_text'])
+with open("harmful.txt", "r") as f:
+    harmful = f.readlines()
+
+    for prompt in harmful:
+        print('===')
+        print(prompt)
+        print('---')
+        conversation=[]
+        conversation.append({"role": "user", "content": prompt})
+        toks = tokenizer.apply_chat_template(conversation=conversation,
+            add_generation_prompt=True, return_tensors="pt")
+
+        gen = model.generate(toks.to(model.device), streamer=streamer, max_new_tokens=200)
+
+        decoded = tokenizer.batch_decode(gen, skip_special_tokens=True)
+        conversation.append({"role": "assistant", "content": decoded})
