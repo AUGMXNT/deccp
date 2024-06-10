a333c0d84edc15b2b6c0232ef8980485bf39170b
lhl <lhl@randomfoo.net>
2024-06-10 17:37:47 +0900

    force org/model-name

--- 04-upload-model-to-hf.py
+++ 04-upload-model-to-hf.py
@@ -4,19 +4,16 @@ import sys
 
 try:
   path = sys.argv[1].rstrip('/')
-  if len(sys.argv) == 3:
-    model_name = sys.argv[2]
-  else:
-    model_name = os.path.basename(path)
+  model_name = sys.argv[2]
 except:
-    print('You should run this with [path-to-upload] [optional-model-name]')
+    print('You should run this with [path-to-upload] [org/model-name]')
     sys.exit(1)
 
 
 api = HfApi()
 try:
     api.create_repo(
-        repo_id=f"augmxnt/{model_name}",
+        repo_id=f"{model_name}",
         # repo_type="model",
         # private=True,
     )
@@ -27,6 +24,6 @@ except:
 os.environ['HF_HUB_ENABLE_HF_TRANSFER'] = "1"
 api.upload_folder(
     folder_path=path,
-    repo_id=f"augmxnt/{model_name}",
+    repo_id=f"{model_name}",
     repo_type='model',
 )
